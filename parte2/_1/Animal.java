6a80feca8dc9e98841ca9ef694293149aa99f8bf
Francesco <f.bignardi@hotmail.it>
2023-04-13 14:42:45 +0200

    Aggiungo esercizi

---
+++ parte2/_1/Animal.java
@@ -0,0 +1,26 @@
+package javaadvanced.parte2._1;
+
+class Animal {
+    private double height, weight;
+
+    public Animal(double height, double weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
